23d10e69c86b51a370d34a4c4f41bae9e83708ad
Martin Beierling-Mutz <mbeierling@gmail.com>
2020-04-04 02:34:38 +0200

    Initial solution

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ["Poppins"]
+    },
+    extend: {
+      colors: {
+        red: "hsl(0, 100%, 74%)",
+        green: {
+          "default": "hsl(154, 59%, 51%)",
+          "light": "hsl(154, 59%, 70%)"
+        },
+        blue: {
+          default: "hsl(248, 32%, 49%)",
+          dark: "hsl(249, 10%, 26%)",
+          grayish: "hsl(246, 25%, 77%)"
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
